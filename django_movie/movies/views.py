62605459753e8df9f0b944b65d1e598fb35858c1
Artem Khrapuzov <tema102@mail.ru>
2023-09-21 13:31:04 +0600

    add star serializer

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -31,6 +31,31 @@ class ReviewCreateView(APIView):
         return Response(status=201)
 
 
+class AddStarRatingView(APIView):
+    """Добавление рейтинга фильму"""
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    def post(self, request):
+        serializer = CreateRatingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(ip=self.get_client_ip(request))
+            return Response(status=201)
+        else:
+            return Response(status=400)
 
 
+
+
+
+
+
+
+
+4
 
