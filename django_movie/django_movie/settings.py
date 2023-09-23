5fb8cad5cad088f5c23ebfd79eaf3d8da2f45c64
Artem Khrapuzov <tema102@mail.ru>
2023-09-23 09:07:30 +0600

    add pagination

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -202,6 +202,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKEND': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 1
 }
 
 DJOSER = {
