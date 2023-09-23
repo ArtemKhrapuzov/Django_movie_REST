61504b33ce3d32c8070b01e0fb9c1ea4bcf10bcd
Artem Khrapuzov <tema102@mail.ru>
2023-09-23 08:53:58 +0600

    replace classes by vievset

--- django_movie/movies/urls.py
+++ django_movie/movies/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
-urlpatterns = [
-    path('movies/', views.MovieListView.as_view()),
-    path('movies/<int:pk>/', views.MovieDetailView.as_view()),
-    path('review/', views.ReviewCreateView.as_view()),
-    path('rating/', views.AddStarRatingView.as_view()),
-    path('actors/', views.ActorListView.as_view()),
-    path('actors/<int:pk>/', views.ActorDetailView.as_view()),
-]
-
+urlpatterns = format_suffix_patterns([
+    path("movie/", views.MovieViewSet.as_view({'get': 'list'})),
+    path("movie/<int:pk>/", views.MovieViewSet.as_view({'get': 'retrieve'})),
+    path("review/", views.ReviewCreateViewSet.as_view({'post': 'create'})),
+    path("rating/", views.AddStarRatingViewSet.as_view({'post': 'create'})),
+    path('actor/', views.ActorsViewSet.as_view({'get': 'list'})),
+    path('actor/<int:pk>/', views.ActorsViewSet.as_view({'get': 'retrieve'})),
+])
